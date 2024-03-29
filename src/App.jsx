d3d1ecd7b378c318429cf9aae418100c9add9741
Vicky <47.efilm.infobibliotecas@gmail.com>
2024-03-29 16:31:40 +0100

    añadir addToCart

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ import {db } from './data/db'
 
 
 function App() {
-    
-
     const [data] = useState(db)
     const [cart, setCart] = useState([])
 
-
+    const addToCart(item) {
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if(itemExists >= 0 ) { // existe en el carrito
+          if(cart[itemExists].quantity >= MAX_ITEMS) return
+          const updatedCart = [...cart]
+          updatedCart[itemExists].quantity++
+          setCart(updatedCart)
+        } else {
+          item.quantity = 1
+          setCart([...cart, item])
+        }
+      }
   return (
     <>
         <Header />
